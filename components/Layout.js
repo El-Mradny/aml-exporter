bcb90b9e755fe7923d3cd5f91bae5e9fd93dee7b
El-Mradny <ahmed.elmradny@gmail.com>
2023-02-13 00:22:56 +0200

    Create Cart page and add heroicon dependency

--- components/Layout.js
+++ components/Layout.js
@@ -1,36 +1,47 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import Head from 'next/head';
 import Link from 'next/link';
-export default function Layout({ title, children }) {
-  return (
-    <>
-      <Head>
-        <title>{title ? title + ' - AML-Exporter' : 'AML-Exporter'}</title>
-        <meta name="description" content="Ecommerce Website" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <div className="flex min-h-screen flex-col justify-between">
-        <header>
-          <nav className="flex h-12 items-center px-4 justify-between shadow-md">
-            <Link href={'/'} className="text-lg font-bold">
-              AML Exporter
-            </Link>
-            <div>
-              <Link href={'/cart'} className="p-2">
-                Cart
-              </Link>
-              <Link href={'/login'} className="p-2">
-                Login
-              </Link>
+import { Store } from '@/utils/Store';
+
+export default function Layout({title, children}) {
+    const {state} = useContext(Store);
+    const {cart} = state;
+    return (
+        <>
+            <Head>
+                <title>{title ? title + ' - AML-Exporter' : 'AML-Exporter'}</title>
+                <meta name="description" content="Ecommerce Website"/>
+                <meta name="viewport" content="width=device-width, initial-scale=1"/>
+                <link rel="icon" href="/favicon.ico"/>
+            </Head>
+            <div className="flex min-h-screen flex-col justify-between">
+                <header>
+                    <nav className="flex h-12 items-center px-4 justify-between shadow-md">
+                        <Link href={'/'} className="text-lg font-bold">
+                            AML Exporter
+                        </Link>
+                        <div>
+                            <Link href={'/cart'} className="p-2">
+
+                                Cart
+                                {cart.cartItems.length > 0 && (
+                                    <span className="ml-1 rounded-full bg-red-600 px-2 py-1 text-xs font-bold text-white">
+                                        {cart.cartItems.reduce((a, c) => a + c.quantity, 0)}
+                                    </span>
+                                )}
+
+                            </Link>
+                            <Link href={'/login'} className="p-2">
+                                Login
+                            </Link>
+                        </div>
+                    </nav>
+                </header>
+                <main className="container m-auto mt-4 px-4">{children}</main>
+                <footer className="flex h-10 justify-center items-center shadow-inner">
+                    <p>Copyright © 2022 AML Exporter</p>
+                </footer>
             </div>
-          </nav>
-        </header>
-        <main className="container m-auto mt-4 px-4">{children}</main>
-        <footer className="flex h-10 justify-center items-center shadow-inner">
-          <p>Copyright © 2022 AML Exporter</p>
-        </footer>
-      </div>
-    </>
-  );
+        </>
+    );
 }
